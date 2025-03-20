598977f7fac2043e725cdc00961b8a5875e1394b
ChuyZip <jesusrosalesromero@icloud.com>
2025-03-20 10:09:47 -0700

    progreso del examen

---
+++ src/entidades/Articulo.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import enums.Categoria;
+
+/**
+ *
+ * @author rosales
+ */
+public class Articulo {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the precio
+     */
+    public Double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+        //nombre:string, precio:double, categoria: Categoria
+
+    private String nombre;
+    
+    private Double precio;
+    
+    private Categoria categoria;
+    
+  
+}
---
+++ src/entidades/Venta.java
@@ -0,0 +1,126 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import enums.Categoria;
+import enums.TipoVenta;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author rosales
+ */
+public class Venta {
+
+    /**
+     * @return the calcularSubtotal
+     */
+    public int getCalcularSubtotal() {
+        return calcularSubtotal;
+    }
+
+    /**
+     * @param calcularSubtotal the calcularSubtotal to set
+     */
+    public void setCalcularSubtotal(int calcularSubtotal) {
+        this.calcularSubtotal = calcularSubtotal;
+    }
+    // Creamos los métodos get y set para cada atributo
+    /**
+     * @return the cliente
+     */
+    public String getCliente() {
+        return cliente;
+    }
+
+    /**
+     * @param cliente the cliente to set
+     */
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    /**
+     * @return the subtotal
+     */
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    /**
+     * @param subtotal the subtotal to set
+     */
+    public void setSubtotal(Double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    /**
+     * @return the total
+     */
+    public Double getTotal() {
+        return total;
+    }
+
+    /**
+     * @param total the total to set
+     */
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public TipoVenta getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(TipoVenta tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the articulos
+     */
+    public List<Articulo> getArticulos() {
+        return articulos;
+    }
+
+    /**
+     * @param articulos the articulos to set
+     */
+    public void setArticulos(List<Articulo> articulos) {
+        this.articulos = articulos;
+    }
+    //cliente:string, tipo:TipoVenta, subtotal:double, descuento: double, total:double, articulos:List<Articulo>
+    
+    private String cliente;
+    
+    private Double subtotal;
+    
+    private Double total;
+    
+    private TipoVenta tipo;
+    
+    private List<Articulo> articulos;
+    
+    private int calcularSubtotal;
+    
+    
+     
+    public void calcularTotal() {}
+
+        for(Articulo a : articulos){
+    
+        if (a.getCategoria() == Categoria.ROPA){
+            
+        } else {
+    }
+    
+
